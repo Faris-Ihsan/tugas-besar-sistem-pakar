7890df4449a9926d79250a047719ca9ef2184ec6
Faris-Ihsan <farisihsan32@yahoo.co.id>
2021-06-27 22:37:58 +0700

    add form (text)

--- siskar_app/views.py
+++ siskar_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    form = forms.FormName()
+    return render(request,'index.html', {'form':form})
