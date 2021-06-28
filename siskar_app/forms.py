95d2a994168ddb701aec271267c05dfbbe899d22
Faris-Ihsan <farisihsan32@yahoo.co.id>
2021-06-28 11:32:39 +0700

    add RadioSelect form & assign input into array

--- siskar_app/forms.py
+++ siskar_app/forms.py
@@ -1,6 +1,25 @@
 from django import forms
 
-CHOICES = [('1','Ya'),('0','Tidak')]
+CHOICES = [(1,'Ya'),(0,'Tidak')]
 
 class FormName(forms.Form):
-    attrib_1 = forms.CharField(label='1. Gejala1', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_1 = forms.CharField(label='1. Gangguan pernapasan', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_2 = forms.CharField(label='2. Demam', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_3 = forms.CharField(label='3. Batuk Kering', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_4 = forms.CharField(label='4. Sakit tenggorokan', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_5 = forms.CharField(label='5. Hidung berlendir', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_6 = forms.CharField(label='6. Pengidap Asthma', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_7 = forms.CharField(label='7. Memiliki riwayat penyakit paru kronis', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_8 = forms.CharField(label='8. Sakit kepala', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_9 = forms.CharField(label='9. Memiliki riwayat sakit jantung', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_10 = forms.CharField(label='10. Memiliki riwayat Diabetes', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_11 = forms.CharField(label='11. Memiliki riwayat Hipertensi', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_12 = forms.CharField(label='12. Mengalami kelelahan', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_13 = forms.CharField(label='13. Memiliki sakit Lambung', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_14 = forms.CharField(label='14. Sedang dalam perjalanan ke luar kota/negeri', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_15 = forms.CharField(label='15. Pernah melakukan kontak dengan pasien COVID', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_16 = forms.CharField(label='16. Melakukan kegiatan di tempat umum', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_17 = forms.CharField(label='17. Ada anggota keluarga yang Melakukan kegiatan di tempat umum?', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_18 = forms.CharField(label='18. Menggunakan Masker', widget=forms.RadioSelect(choices=CHOICES))
+    attrib_19 = forms.CharField(label='19. Rajin menggunakan hand sanitizer?', widget=forms.RadioSelect(choices=CHOICES))
+
--- siskar_app/views.py
+++ siskar_app/views.py
@@ -3,6 +3,19 @@ from django.http import HttpResponse
 from . import forms
 
 # Create your views here.
+
+
 def index(request):
+    a=[]
     form = forms.FormName()
+
+    if request.method=='POST':
+        form=forms.FormName(request.POST)
+
+        if form.is_valid():
+            for i in range(19):
+                a.append(form.cleaned_data['attrib_'+str(i+1)])
+            print(a)
+
+            
     return render(request,'index.html', {'form':form})
