a13ec9f048dede7a6c04a4032deb2f1c6f46c941
Faris-Ihsan <farisihsan32@yahoo.co.id>
2021-07-03 22:40:27 +0700

    import modelsloader class

--- siskar_app/views.py
+++ siskar_app/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import forms
+from siskar_app.datamodels import modelsloader
 
 # Create your views here.
 
 
 def index(request):
-    a=[]
+    inputan=[]
     form = forms.FormName()
 
     if request.method=='POST':
         form=forms.FormName(request.POST)
 
         if form.is_valid():
-            for i in range(19):
-                a.append(int(form.cleaned_data['attrib_'+str(i+1)]))
-            print(a)
+            for i in range(20):
+                inputan.append(int(form.cleaned_data['attrib_'+str(i+1)]))
+        
+        hasil = modelsloader.loadmodels(inputan)
+        
+        if hasil == 1:
+            print("positif")
+        else:
+            print("negatif")
 
             
     return render(request,'index.html', {'form':form})
