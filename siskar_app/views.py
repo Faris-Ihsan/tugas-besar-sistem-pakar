ce3ec5ee3eba6a3ea0ab0016981946ebab29d650
Faris-Ihsan <farisihsan32@yahoo.co.id>
2021-07-04 00:03:54 +0700

    add messages alert to views.py

--- siskar_app/views.py
+++ siskar_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import forms
 from siskar_app.datamodels import modelsloader
+from django.contrib import messages
 
 # Create your views here.
 
@@ -17,12 +18,13 @@ def index(request):
             for i in range(20):
                 inputan.append(int(form.cleaned_data['attrib_'+str(i+1)]))
         
-        hasil = modelsloader.loadmodels(inputan)
+        hasil = modelsloader.loadmodels(inputan)        
         
         if hasil == 1:
-            print("positif")
+            messages.warning(request, 'Positif COVID-19')
         else:
-            print("negatif")
+            messages.warning(request, 'Negatif COVID-19')
+        
 
             
     return render(request,'index.html', {'form':form})
